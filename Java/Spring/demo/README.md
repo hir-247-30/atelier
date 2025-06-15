fbc57f4af98b939c32eff09af0deaa26b3a1ede7
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2025-06-15 18:17:31 +0900

    feat: Spring素振り

--- Java/Spring/demo/README.md
+++ Java/Spring/demo/README.md
@@ -2,6 +2,13 @@
 
 - Java24
 
+## ビルドと起動
+
+```
+./gradlew build
+./gradlew bootRun
+```
+
 ## 参考にしたものとか
 
 - https://qiita.com/high_high/items/263cfcbd712ada6cddd3
---
+++ Java/Spring/demo/src/main/java/com/example/demo/web/HomeRestController.java
@@ -0,0 +1,53 @@
+package com.example.demo.web;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeRestController {
+    record TaskItem(
+            String id,
+            String task,
+            String deadline,
+            boolean done
+    ) {}
+
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    @GetMapping("/resthello")
+    public String hello() {
+        return """
+                Hello It works!
+                現在時刻は%sです。
+                """.formatted(LocalDateTime.now());
+    }
+
+    // 実際は @PutMapping とすること
+    @GetMapping("/restadd")
+    String addItem(
+            @RequestParam("task") String task,
+            @RequestParam("deadline") String deadline
+    ) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "タスクを追加しました。";
+    }
+
+    @GetMapping("/restlist")
+    String listItems() {
+        String result = taskItems.stream()
+                .map(TaskItem::toString)
+                .collect(Collectors.joining(", "));
+
+        return result;
+    }
+}
