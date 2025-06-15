00957e43ef6366fd3acd69481bf64e9e4ff8cb66
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2025-06-15 19:42:01 +0900

    chore: SpringサンプルDB接続

--- Java/Spring/demo/src/main/java/com/example/demo/web/HomeRestController.java
+++ Java/Spring/demo/src/main/java/com/example/demo/web/HomeRestController.java
@@ -1,11 +1,14 @@
 package com.example.demo.web;
 
+import com.example.demo.web.TaskListDao;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +22,15 @@ public class HomeRestController {
             boolean done
     ) {}
 
+    private final TaskListDao dao;
     private List<TaskItem> taskItems = new ArrayList<>();
 
-    @GetMapping("/resthello")
+    @Autowired
+    HomeRestController (TaskListDao dao) {
+        this.dao = dao;
+    }
+
+    @GetMapping("/hello")
     public String hello() {
         return """
                 Hello It works!
@@ -29,22 +38,45 @@ public class HomeRestController {
                 """.formatted(LocalDateTime.now());
     }
 
-    // 実際は @PutMapping とすること
-    @GetMapping("/restadd")
-    String addItem(
+    // 実際は @PostMapping とすること
+    @GetMapping("/add")
+    String addItem (
             @RequestParam("task") String task,
             @RequestParam("deadline") String deadline
     ) {
         String id = UUID.randomUUID().toString().substring(0, 8);
         TaskItem item = new TaskItem(id, task, deadline, false);
-        taskItems.add(item);
+        this.dao.add(item);
 
         return "タスクを追加しました。";
     }
 
-    @GetMapping("/restlist")
-    String listItems() {
-        String result = taskItems.stream()
+    // 実際は @PutMapping とすること
+    @GetMapping("/update")
+    String updateItem (
+            @RequestParam("id") String id,
+            @RequestParam("task") String task,
+            @RequestParam("deadline") String deadline,
+            @RequestParam("done") boolean done
+    ) {
+        TaskItem taskItem = new TaskItem(id, task, deadline, done);
+        this.dao.update(taskItem);
+
+        return "タスクを更新しました。";
+    }
+
+    // 実際は @DeleteMapping とすること
+    @GetMapping("/delete")
+    String deleteItems (@RequestParam("id") String id) {
+        this.dao.delete(id);
+
+        return "タスクを削除しました。";
+    }
+
+    @GetMapping("/list")
+    String listItems () {
+        List<TaskItem> items = this.dao.findAll();
+        String result = items.stream()
                 .map(TaskItem::toString)
                 .collect(Collectors.joining(", "));
 
